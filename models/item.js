e1b73f755bc0c9526d045d7f20a26d3e5271734d
Fadhli Sani <fadhli112@hotmail.com>
2018-12-08 08:04:25 +0800

    Added functions in item model

--- models/item.js
+++ models/item.js
@@ -7,19 +7,19 @@ const itemSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
-        require: true
+        require: "Please enter price number"
     },
     description: {
         type: String
     },
     user_id: {
         type: String,
-        require: true
+        require: "Please enter user id"
     },
     location: {
         type: String
     },
-    date_post: {
+    date_created: {
         type: Date
     },
     category: {
@@ -28,3 +28,40 @@ const itemSchema = new mongoose.Schema({
 });
 
 const Item = module.exports = mongoose.model('Item', itemSchema);
+
+// Get all items
+module.exports.findAllItems = function(callback) {
+    Item.find({}, callback);
+}
+
+// Get item by id
+module.exports.findItemById = function(id, callback) {
+    Item.findById(id, callback);
+}
+
+// Get items by category
+module.exports.findAllItemsByCategory = function(category, callback) {
+    const query = {category: category}
+    Item.find(query, callback);
+}
+
+// Get item by name
+module.exports.findItemByName = function(name, callback) {
+    const query = {name: name}
+    Item.find(query, callback);
+}
+
+// add new item
+module.exports.addNewItem = function() {
+
+}
+
+// update item
+module.exports.updateItem = function(id, callback) {
+
+}
+
+// delete item
+module.exports.deleteItem = function(id, callback) {
+
+}
