68d3c2098ead81e7532cb6c85b2183d5c586a53a
Fadhli Sani <fadhli112@hotmail.com>
2018-12-08 08:03:54 +0800

    Added mongoose package

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const config = require('./config/database');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.database);
 
 const app = express();
 
 const items = require('./routes/items');
+//const users = require('./routes/users');
 
-const PORT = 3000;
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/items', items);
+// app.use('/users', users);
 
 app.get('/', (req, res) => {
     res.send('it works!');
 });
 
-app.listen(PORT, () => {
-    console.log("Server is running on port: "+PORT);
+app.listen(config.port, () => {
+    console.log("Server is running on port: "+config.port);
 })
