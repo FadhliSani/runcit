a78b8aa3a3a288e85ae8cb2fd6f62f9495aa6761
Fadhli Sani <fadhli112@hotmail.com>
2018-12-13 21:54:34 +0800

    Setting up MDBootstrap

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModulesPro.forRoot()
   ],
-  providers: [],
+  providers: [
+    MDBSpinningPreloader
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
