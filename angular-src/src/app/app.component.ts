cbef1ed869d433ed21c53e6170a36e6c9af4ee93
Fadhli Sani <fadhli112@hotmail.com>
2018-12-15 21:04:45 +0800

    Added home component to app module

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Runcitku!';
+  title = 'Runcitku';
+
+  loginFormModalEmail = new FormControl('', Validators.email);
+  loginFormModalPassword = new FormControl('', Validators.required);
 }
